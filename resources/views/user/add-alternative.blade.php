7e3e5b148dfaf36b5fe0aba45a3e933c58a4d560
Godlife-Hindom <158173000+Godlife-Hindom@users.noreply.github.com>
2025-06-22 05:43:54 +0800

    Update add-alternative.blade.php

--- resources/views/user/add-alternative.blade.php
+++ resources/views/user/add-alternative.blade.php
@@ -307,8 +307,6 @@
     </div>
 </div>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
 
 <style>
