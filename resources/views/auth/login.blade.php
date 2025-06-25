d786994a206c83cc15b73c50d95a82a041cc3153
User <https://github.com/Godlife-Hindom>
2025-06-25 13:12:29 +0800

    Update beberapa tampilan  user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -235,9 +235,11 @@
 
             <!-- Forgot + Submit -->
             <div class="flex flex-col sm:flex-row items-center justify-between mt-4 sm:mt-6 space-y-3 sm:space-y-0 animate-fade-in delay-1000">
-                <x-primary-button class="w-full sm:w-auto sm:ml-3 px-6 py-2 sm:py-3 text-sm sm:text-base font-semibold bg-emerald-600 hover:bg-emerald-700 focus:bg-emerald-700 active:bg-emerald-900 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 transition-all duration-300 rounded-xl btn-hover">
+            <div class="flex justify-center">
+                <x-primary-button class="w-full sm:w-auto px-6 py-2 sm:py-3 text-sm sm:text-base font-semibold bg-emerald-600 hover:bg-emerald-700 focus:bg-emerald-700 active:bg-emerald-900 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 transition-all duration-300 rounded-xl btn-hover">
                     {{ __('Login') }}
                 </x-primary-button>
+            </div>
             </div>
         </form>
 
