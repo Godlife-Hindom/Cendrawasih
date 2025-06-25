5637c966adc608c7ce6fa57a4ac188eabcec9b14
User <https://github.com/Godlife-Hindom>
2025-06-25 16:28:11 +0800

    Update beberapa tampilan  user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register SPK Cendrawasih</title>
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/pp.png') }}" type="image/png">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @keyframes fadeIn {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>SPK Cendrawasih</title>
+  <!-- Favicon -->
+  <link rel="icon" href="{{ asset('images/pp.png') }}" type="image/png">
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
