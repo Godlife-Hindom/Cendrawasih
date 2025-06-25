af791f77815418bbb4ea9aea4274c1f68a728ca5
User <https://github.com/Godlife-Hindom>
2025-06-25 14:31:08 +0800

    Update beberapa tampilan  user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -140,7 +140,7 @@
     }
     
     .mobile-overlay {
-      background-color: rgba(0, 0, 0, 0.5);
+      background-color: rgba(255, 255, 255, 0.6); /* ini transparan */
       opacity: 0;
       visibility: hidden;
       transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
